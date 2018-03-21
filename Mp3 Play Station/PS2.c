55f61bf0beafc2aafe366997b81156dc8ef157b5
QuinnBast <Quinn.Bast@hotmail.com>
2018-03-20 23:57:32 -0600

    *PS2 Controller working

--- Mp3 Play Station/PS2.c
+++ Mp3 Play Station/PS2.c
@@ -4,6 +4,14 @@
  * Version: V1.00
  * Authors: Quinn Bast
  *
+ *	Notes:
+ *	Brown = Data (to Micro) = To PB14 and pull-up resistor from 5V
+ *	Red = Power = 5V
+ *	Orange = Command (from micro) To PB15
+ *	Yellow = Chip Select = PB12
+ *	Green = Attention = Resistor to 5V
+ *	Blue	=	Clock = PB13
+ *	Black = GND
  *
  *
  *----------------------------------------------------------------------------
@@ -12,12 +20,12 @@
  **********
  *******************************************************************/
  #include "C:\Users\Quinn\Dropbox\College\Semester 11 2018 Winter\ENEL 387\ENEL-387-Lab\Libraries\GPIO_lib.h"
-  #include "C:\Users\Quinn\Dropbox\College\Semester 11 2018 Winter\ENEL 387\ENEL-387-Lab\Libraries\LCD.h"
+ #include "C:\Users\Quinn\Dropbox\College\Semester 11 2018 Winter\ENEL 387\ENEL-387-Lab\Libraries\LCD.h"
  #include "stm32f10x.h"
  #include <stdint.h>
  #include <math.h>
  #include <assert.h>
- 
+
 
   uint8_t readWrite(uint8_t data){
 	 
@@ -38,22 +46,26 @@
  }
  
  void PS2Init(){
+	 	 uint8_t ret = 0x00;
 	 
 		RCC->APB1ENR |= RCC_APB1ENR_SPI2EN;		//Enable the clock for the SPI interface
-	 
 		RCC->APB2ENR |= RCC_APB2ENR_IOPBEN;		//Enable the clock for Port B IO
 		RCC->APB2ENR |= RCC_APB2ENR_AFIOEN;		//Enable alternate function clock
 	 
 	  //Set PB15 and PB 13 to alternate function outputs.
-    GPIOB->CRH |= GPIO_CRH_CNF15_1 | GPIO_CRH_CNF13_1 | GPIO_CRH_MODE13  |GPIO_CRH_MODE15 ;
+    GPIOB->CRH |= GPIO_CRH_CNF15_1 | GPIO_CRH_CNF13_1 | GPIO_CRH_MODE13 | GPIO_CRH_MODE15 ;
     GPIOB->CRH &= ~GPIO_CRH_CNF15_0 & ~GPIO_CRH_CNF13_0 ;
 	 
 		//Set PB14 to floating input (default state, no change required)
+	  GPIOB->CRH &= ~GPIO_CRH_MODE14 & ~GPIO_CRH_CNF14_0;
+	  GPIOB->CRH |= GPIO_CRH_CNF14_1;
+	 
 		//Set PB12 to GPIO out
 		GPIOB->CRH |= GPIO_CRH_MODE12;
+	  GPIOB->CRH &= ~GPIO_CRH_CNF12;
 	 
 		//Make PB12 high so no data is being sent over SPI on init.
-		GPIOB->ODR = GPIO_ODR_ODR12;
+		GPIOB->ODR |= GPIO_ODR_ODR12;
 	 
 	 
 		//Init SPI.
@@ -64,40 +76,31 @@
 		SPI2->CR1 |= SPI_CR1_MSTR;	//This device is the master
 		
 		//Device specific configurations.
-		SPI2->CR1 &= ~SPI_CR1_CPHA;	//Second clock edge is the data transfer.
+		SPI2->CR1 |= SPI_CR1_CPHA;	//Second clock edge (high->low) is the data transfer.
 		SPI2->CR1 |= SPI_CR1_CPOL;	//Clock high when idle
-		SPI2->CR1 &= ~SPI_CR1_LSBFIRST;	//MSB sent in first.
-		SPI2->CR1 |= SPI_CR1_BR_2;	//Set the baud rate to 125KHz (24MHz/x = 125KHz) x = 192. Next power of 2 is 256 = 0x110
+		SPI2->CR1 |= SPI_CR1_LSBFIRST;	//LSB sent in first.
+		SPI2->CR1 |= SPI_CR1_BR_2 | SPI_CR1_BR_1 | SPI_CR1_BR_0;	//Set the baud rate to 125KHz (24MHz/x = 125KHz) x = 192. Next power of 2 is 256 = 0x110
 		
 		//Enable the SPI interface
 		SPI2->CR1 |= SPI_CR1_SPE;
 		
 		delay(600*15);
 		
+	
 		//Wake the device by sending 3 short polls.
 	for(int i=0; i<3; i++)
 	{
 		GPIOB->BSRR = GPIO_BSRR_BR12;	//Attention LOW
-		/*
-		assert(readWrite(0x01) == 0xFF);
-		assert(readWrite(0x42) == 0x41); //41
-		assert(readWrite(0x00) == 0x5A); //5a
-		assert(readWrite(0xFF) == 0xFF);
-		assert(readWrite(0xFF) == 0xFF);
-		*/
-		delay(60);
 		readWrite(0x01);
 		readWrite(0x42);
 		readWrite(0x00);
 		readWrite(0x00);
 		readWrite(0x00);
 		GPIOB->BSRR = GPIO_BSRR_BS12;	//Attention HIGH
-		delay(60);
 	}
 	
 	//Enter Config Mode
 	GPIOB->BSRR = GPIO_BSRR_BR12;	//Attention LOW
-	delay(60);
 	readWrite(0x01);
 	readWrite(0x43);
 	readWrite(0x00);
@@ -105,11 +108,9 @@
 	readWrite(0x00);
 	GPIOB->BSRR = GPIO_BSRR_BS12;	//Attention HIGH
 	
-	delay(60);
 	
 	//Enable Analog Controls
 	GPIOB->BSRR = GPIO_BSRR_BR12;	//Attention LOW
-	delay(60);
 	readWrite(0x01);
 	readWrite(0x44);
 	readWrite(0x00);
@@ -121,11 +122,9 @@
 	readWrite(0x00);
 	GPIOB->BSRR = GPIO_BSRR_BS12;	//Attention HIGH
 	
-	delay(60);
 	
 	//Exit Config Mode
 	GPIOB->BSRR = GPIO_BSRR_BR12;	//Attention LOW
-	delay(60);
 	readWrite(0x01);
 	readWrite(0x43);
 	readWrite(0x00);
@@ -136,26 +135,26 @@
 	readWrite(0x5A);
 	readWrite(0x5A);
 	GPIOB->BSRR = GPIO_BSRR_BS12;	//Attention HIGH
+	
 }
  
- uint8_t* pollController(){
-	 static uint8_t ret[6] = {0};
+ uint16_t* pollController(){
+	 static uint16_t ret[3] = {0};
+	 ret[0] = 0x0000;
+	 ret[1] = 0x0000;
+	 ret[2] = 0x0000;
 	 uint8_t command[9] = {0x01, 0x42, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
 	 
 	 GPIOB->BSRR = GPIO_BSRR_BR12;	//Attention LOW
-	 delay(60);
 	 readWrite(command[0]);
 	 readWrite(command[1]);
 	 readWrite(command[2]);
-	 ret[0] = readWrite(command[3]);
-	 ret[1] = readWrite(command[4]);
-	 ret[2] = readWrite(command[5]);
-	 ret[3] = readWrite(command[6]);
-	 ret[4] = readWrite(command[7]);
-	 ret[5] = readWrite(command[8]);
-	 
-	 hexToLCD(((ret[0] << 8*3) | (ret[1] << 8*2)), 0, 0);
-	 hexToLCD(((ret[2] << 8*3) | (ret[3] << 8*2) | (ret[4] << 8*1) | (ret[5])), 1, 0);
+	 ret[0] |= readWrite(command[3]) << 8;
+	 ret[0] |= readWrite(command[4]);
+	 ret[1] |= readWrite(command[5]) << 8;
+	 ret[1] |= readWrite(command[6]);
+	 ret[2] |= readWrite(command[7]) << 8;
+	 ret[2] |= readWrite(command[8]);
 	 
 	 GPIOB->BSRR = GPIO_BSRR_BS12;	//Attention HIGH
 	 return ret;
--- Mp3 Play Station/PS2.h
+++ Mp3 Play Station/PS2.h
@@ -14,11 +14,28 @@
  #include "stm32f10x.h"
  #include <stdint.h>
  
+ #define PS2_L2_BUTTON 0x0001
+ #define PS2_R2_BUTTON 0x0002
+ #define PS2_L1_BUTTON 0x0004
+ #define PS2_R1_BUTTON 0x0008
+ #define PS2_TRIANGLE_BUTTON 0x0010
+ #define PS2_CIRCLE_BUTTON 0x0020
+ #define PS2_X_BUTTON 0x0040
+ #define PS2_SQUARE_BUTTON 0x0080
+ #define PS2_SELECT_BUTTON 0x0100
+ #define PS2_L3_BUTTON 0x0200
+ #define PS2_R3_BUTTON 0x0400
+ #define PS2_START_BUTTON 0x0800
+ #define PS2_UP_BUTTON 0x1000
+ #define PS2_RIGHT_BUTTON 0x2000
+ #define PS2_DOWN_BUTTON 0x4000
+ #define PS2_LEFT_BUTTON 0x8000
+ 
  //initialize the SPI interface
  void PS2Init(void);
  
  //send data over SPI 
- uint8_t* pollController();
+ uint16_t* pollController();
  
  //Send and read data from the controller
  uint8_t readWrite(uint8_t);
